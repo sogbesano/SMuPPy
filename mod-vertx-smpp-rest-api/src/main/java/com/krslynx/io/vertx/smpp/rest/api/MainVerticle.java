989cb9922e77db98a63ed1119b197b0e6619a573
Christopher Burke <chris@krslynx.com>
2015-01-13 20:28:04 +0000

    Cleaned up the MainVerticle; added JavaDocs.

--- mod-vertx-smpp-rest-api/src/main/java/com/krslynx/io/vertx/smpp/rest/api/MainVerticle.java
+++ mod-vertx-smpp-rest-api/src/main/java/com/krslynx/io/vertx/smpp/rest/api/MainVerticle.java
@@ -3,8 +3,14 @@ package com.krslynx.io.vertx.smpp.rest.api;
 import com.krslynx.io.vertx.smpp.rest.api.routing.RouteManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.platform.Verticle;
 
+/**
+ * <p>Main entry point to the application.</p>
+ *
+ * @author Christopher Burke
+ */
 public class MainVerticle extends Verticle {
 
     /**
@@ -12,8 +18,16 @@ public class MainVerticle extends Verticle {
      */
     private static Logger log = LoggerFactory.getLogger(MainVerticle.class);
 
+    /**
+     * <p>Code executed when application is started</p>
+     */
     @Override
     public void start() {
-        vertx.createHttpServer().requestHandler(new RouteManager()).listen(8080);
+
+        final JsonObject appConfig = container.config();
+        log.info("Deploying MongoDB Persistor with config: " + appConfig.encodePrettily());
+        container.deployModule("io.vertx~mod-mongo-persistor~2.1.1", appConfig.getObject("mongo-persistor"));
+
+        vertx.createHttpServer().requestHandler(new RouteManager(vertx)).listen(8080);
     }
 }
